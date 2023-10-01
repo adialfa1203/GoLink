5c618558e18e275ae86b754dfdadb4850b31a36f
adi alfa <adialfa1203@gmail.com>
2023-10-01 15:25:51 +0700

    fixing error in acess microsite

--- app/Http/Controllers/MicrositeController.php
+++ app/Http/Controllers/MicrositeController.php
@@ -128,7 +128,7 @@ class MicrositeController extends Controller
         $link_microsite = str_replace(' ', '-', $request->link_microsite);
         ShortUrl::findOrFail($short_id)->update([
             'url_key' => $link_microsite,
-            'default_short_url' => env('APP_URL')."go.microsite/" . $link_microsite,
+            'default_short_url' => env('APP_URL')."/go.microsite/" . $link_microsite,
         ]);
 
 
