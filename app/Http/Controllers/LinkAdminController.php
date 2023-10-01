c3481b1f0f5466a803f9f34801911373ece1f290
adi alfa <adialfa1203@gmail.com>
2023-10-01 10:53:49 +0700

    error page

--- app/Http/Controllers/LinkAdminController.php
+++ app/Http/Controllers/LinkAdminController.php
@@ -11,47 +11,45 @@ class LinkAdminController extends Controller
     public function linkAdmin()
     {
         $data = User::where('is_banned', 0)->role('user')->get();
-        
+        //Menghitung total user
         $totalUser = User::where('email', '!=', 'admin@gmail.com')
-                        ->where('is_banned', '!=', '1')
-                        ->count();
-        
+                    ->where('is_banned', '!=', '1')
+                    ->count();
+        //Menghitung total url
         $totalUrl = ShortUrl::where('archive', '!=', 'yes')->count();
-        
+        //Menghitung total pengunjung
         $totalVisits = ShortURLVisit::query()
-                            ->whereRelation('shortURL', 'archive', '!=', 'yes')
-                            ->count();
+                        ->whereRelation('shortURL', 'archive', '!=', 'yes')
+                        ->count();
 
         $totalMicrosite = ShortUrl::whereNotNull('microsite_uuid')->count();
 
+        //Menampilkan data user di dalam tabel
+        // $users = User::where('email', '!=', 'admin@gmail.com')->get();
+        // //Menampilkan total url untuk setiap user
+        // $count = [];
+        // foreach ($users as $user) {
+        //     $count[$user->id] = ShortUrl::where('user_id', $user->id)->count();
+        //
         $users = User::where('email', '!=', 'admin@gmail.com')
-                    ->where('is_banned', '!=', '1')
-                    ->paginate(10);
-        
-        $d = $users;
-
+                ->where('is_banned', '!=', '1')
+                ->paginate(10);
+        $d=$users;
         foreach ($users as $user) {
             $userData[$user->id] = [
                 'total_links' => ShortUrl::where('user_id', $user->id)->whereNull('microsite_uuid')->count(),
                 'total_microsites' => ShortUrl::where('user_id', $user->id)->whereNotNull('microsite_uuid')->count(),
                 'popular_links' => ShortUrl::where('user_id', $user->id)
                     ->withCount('visits')
-                    ->orderByDesc('visits_count') // Mengurutkan berdasarkan jumlah tautan terbanyak
+                    ->orderBy('visits_count', 'desc')
                     ->first(),
                 'popular_microsites' => ShortUrl::where('user_id', $user->id)
                     ->whereNotNull('microsite_uuid')
                     ->withCount('visits')
-                    ->orderByDesc('visits_count') // Mengurutkan berdasarkan jumlah tautan terbanyak
+                    ->orderBy('visits_count', 'desc')
                     ->first(),
             ];
         }
-
-        // Mengurutkan $users berdasarkan jumlah tautan terbanyak
-        $users = $users->sortByDesc(function ($user) use ($userData) {
-            return $userData[$user->id]['total_links'];
-        });
-
-        return view('Admin.Link', compact('d', 'totalUser', 'totalUrl', 'totalVisits', 'users', 'totalMicrosite', 'userData'));
+        return view('Admin.Link', compact('d','totalUser', 'totalUrl', 'totalVisits', 'users', 'totalMicrosite', 'userData'));
     }
-
 }
