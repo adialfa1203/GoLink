5d2041904254512622ba151821bbb7159b5fad8c
adi alfa <adialfa1203@gmail.com>
2023-10-01 21:30:42 +0700

    dashboard

--- resources/views/User/DashboardUser.blade.php
+++ resources/views/User/DashboardUser.blade.php
@@ -84,7 +84,7 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0">Bernada</h4>
+                            <h4 class="mb-sm-0">Beranda</h4>
                         </div>
                     </div>
                 </div>
