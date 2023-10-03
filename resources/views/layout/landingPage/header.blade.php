e8a53815549eaf39075de7c7d2a01b8ac6813a76
jevonkagenou <kichironomercy@gmail.com>
2023-10-03 15:43:14 +0700

    Fixing Bug

--- resources/views/layout/landingPage/header.blade.php
+++ resources/views/layout/landingPage/header.blade.php
@@ -1,55 +1,4 @@
 @auth
-<header>
-    <!-- container start -->
-    <div class="container">
-        <!-- navigation bar -->
-        <nav class="navbar navbar-expand-lg" style="margin-top:-3%">
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('linkbaru.png') }}" alt="image">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon">
-                    <!-- <i class="icofont-navigation-menu ico_menu"></i> -->
-                    <div class="toggle-wrap">
-                        <span class="toggle-bar"></span>
-                    </div>
-                </span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <!-- secondery menu start -->
-                    <li class="nav-item has_dropdown">
-                        <a class="nav-link {{ request()->routeIs('landing.page') ? 'active' : '' }}" href="/">Beranda</a>
-                    </li>
-
-                    <!-- secondery menu start -->
-                    <li class="nav-item has_dropdown">
-                        <a class="nav-link {{ request()->routeIs('short.link') ? 'active' : '' }}" href="/Shortlink">Perpendek Link</a>
-                    </li>
-                    <!-- secondery menu end -->
-
-                    <li class="nav-item has_dropdown">
-                        <a class="nav-link {{ request()->routeIs('microsite.page') ? 'active' : '' }}" href="/Microsite">Situs Mikro</a>
-                    </li>
-                    <li class="nav-item has_dropdown">
-                        <a class="nav-link {{ request()->routeIs('subscribe.page') ? 'active' : '' }}" href="/Subscribe">Berlangganan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link dark_btn" href="/dashboard-user">
-                            Beranda&nbsp;&nbsp;<i class="fa fa-arrow-right"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <!-- navigation end -->
-    </div>
-    <!-- container end -->
-</header>
-    @else
     <header>
         <!-- container start -->
         <div class="container">
@@ -58,9 +7,8 @@
                 <a class="navbar-brand" href="#">
                     <img src="{{ asset('linkbaru.png') }}" alt="image">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
                         <!-- <i class="icofont-navigation-menu ico_menu"></i> -->
                         <div class="toggle-wrap">
@@ -73,23 +21,83 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- secondery menu start -->
                         <li class="nav-item has_dropdown">
-                            <a class="nav-link {{ request()->routeIs('landing.page') ? 'active' : '' }}" href="/">Beranda</a>
+                            <a class="nav-link {{ request()->routeIs('landing.page') ? 'active' : '' }}"
+                                href="/">Beranda</a>
                         </li>
 
                         <!-- secondery menu start -->
                         <li class="nav-item has_dropdown">
-                            <a class="nav-link {{ request()->routeIs('short.link') ? 'active' : '' }}" href="/Shortlink">Perpendek Link</a>
+                            <a class="nav-link {{ request()->routeIs('short.link') ? 'active' : '' }}"
+                                href="/Shortlink">Perpendek Link</a>
                         </li>
                         <!-- secondery menu end -->
 
                         <li class="nav-item has_dropdown">
-                            <a class="nav-link {{ request()->routeIs('microsite.page') ? 'active' : '' }}" href="/Microsite">Situs Mikro</a>
+                            <a class="nav-link {{ request()->routeIs('microsite.page') ? 'active' : '' }}"
+                                href="/Microsite">Situs Mikro</a>
                         </li>
                         <li class="nav-item has_dropdown">
-                            <a class="nav-link {{ request()->routeIs('subscribe.page') ? 'active' : '' }}" href="/Subscribe">Berlangganan</a>
+                            <a class="nav-link {{ request()->routeIs('subscribe.page') ? 'active' : '' }}"
+                                href="/Subscribe">Berlangganan</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link dark_btn" href="/login">Masuk / Daftar</a>
+                            <a class="nav-link dark_btn" href="/dashboard-user">
+                                Beranda&nbsp;&nbsp;<i class="fa fa-arrow-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- navigation end -->
+        </div>
+        <!-- container end -->
+    </header>
+@else
+    <header>
+        <!-- container start -->
+        <div class="container">
+            <!-- navigation bar -->
+            <nav class="navbar navbar-expand-lg" style="margin-top:-3%">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('linkbaru.png') }}" alt="image">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                        <!-- <i class="icofont-navigation-menu ico_menu"></i> -->
+                        <div class="toggle-wrap">
+                            <span class="toggle-bar"></span>
+                        </div>
+                    </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <!-- secondery menu start -->
+                        <li class="nav-item has_dropdown">
+                            <a class="nav-link {{ request()->routeIs('landing.page') ? 'active' : '' }}"
+                                href="/">Beranda</a>
+                        </li>
+
+                        <!-- secondery menu start -->
+                        <li class="nav-item has_dropdown">
+                            <a class="nav-link {{ request()->routeIs('short.link') ? 'active' : '' }}"
+                                href="/Shortlink">Perpendek Link</a>
+                        </li>
+                        <!-- secondery menu end -->
+
+                        <li class="nav-item has_dropdown">
+                            <a class="nav-link {{ request()->routeIs('microsite.page') ? 'active' : '' }}"
+                                href="/Microsite">Situs Mikro</a>
+                        </li>
+                        <li class="nav-item has_dropdown">
+                            <a class="nav-link {{ request()->routeIs('subscribe.page') ? 'active' : '' }}"
+                                href="/Subscribe">Berlangganan</a>
+                        </li>
+                        <li class="nav-item">
+                            <div class="nav-link dark_btn">
+                                <a class="text-dark" href="/login">Masuk /</a><a class="text-dark" href="/register"> Daftar</a>
+                            </div>
                         </li>
                     </ul>
                 </div>
