a9273534e768e0e11e95a184c67be9c79974bdc3
Gmbs <gmbsstok@gmail.com>
2023-10-10 14:50:33 +0700

    Update HelpSupport.blade.php

--- resources/views/HelpSupport/HelpSupport.blade.php
+++ resources/views/HelpSupport/HelpSupport.blade.php
@@ -151,9 +151,11 @@
                                         class="d-flex align-items-center text-muted mb-4 comment-container{{ $key < 1 ? '' : ' hidden' }}">
                                         <div class="flex-shrink-0 me-3">
                                         @if($row->user->profile_picture)
-                        <img class="header-profile-user" src="{{ asset('profile_pictures/' . $row->user->profile_picture) }}" alt="Header Avatar" style="margin-right: 10px; object-fit: cover;">
+                        <img class="header-profile-user" src="{{ asset('profile_pictures/' . $row->user->profile_picture) }}" alt="Header Avatar" width="50px"
+                                                height="50px" style="margin-right: 10px; object-fit: cover;">
                         @else
-                        <img class="header-profile-user" src="{{ asset('default/default.jpg') }}" alt="Default Avatar" style="margin-right: 10px; object-fit: cover;">
+                        <img class="header-profile-user" src="{{ asset('default/default.jpg') }}" alt="Default Avatar" width="50px"
+                                                height="50px" style="margin-right: 10px; object-fit: cover;">
                         @endif
                         <!-- <img src="{{ asset('profile_pictures/' . $row->user->profile_picture) }}"
                                                 alt="{{ $users->row }}" class="avatar-sm rounded" width="50px"
