9ecd7b5fc236e7955d5b974d49482c343c549649
firaelva <109937514+firaelva@users.noreply.github.com>
2023-10-10 10:07:30 +0700

    validasi alamat

--- app/Http/Controllers/MicrositeController.php
+++ app/Http/Controllers/MicrositeController.php
@@ -165,21 +165,21 @@ class MicrositeController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'nullable|string|max:35',
             'name_microsite' => 'nullable|string|max:35',
-            'description' => 'nullable|string|max:200',
+            'description' => 'nullable|string|max:300',
             'company_name' => 'required|string|max:35', // Menghapus 'nullable'
-            'company_address' => 'required|string|max:35', // Menghapus 'nullable'
+            'company_address' => 'required|string|max:100', // Menghapus 'nullable'
             'button_link.*' => 'required|string|url',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
         ], [
             'name_microsite' => 'Kolom nama microsite tidak boleh lebih besar dari 35 karakter.',
-            'description' => 'Deskripsi tidak boleh lebih besar dari 200 karakter.',
+            'description' => 'Deskripsi tidak boleh lebih besar dari 300 karakter.',
             'image.image' => 'Kolom harus berupa gambar!',
             'button_link.*.required' => 'Kolom ini wajib diisi!',
             'button_link.*.url' => 'URL tidak valid.',
             'company_name.required' => 'Nama perusahaan wajib diisi!', // Menambah pesan validasi baru
             'company_name.max' => 'Nama perusahaan tidak boleh lebih besar dari 35 karakter.', // Memindahkan pesan validasi max ke sini
             'company_address.required' => 'Alamat perusahaan wajib diisi!', // Menambah pesan validasi baru
-            'company_address.max' => 'Alamat perusahaan tidak boleh lebih besar dari 35 karakter.', // Memindahkan pesan validasi max ke sini
+            'company_address.max' => 'Alamat perusahaan tidak boleh lebih besar dari 100 karakter.', // Memindahkan pesan validasi max ke sini
         ]);
 
         if ($validator->fails()) {
