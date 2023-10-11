06343833b79fe97536b9fcefacfeb0399f42e81c
Gmbs <gmbsstok@gmail.com>
2023-10-11 13:12:34 +0700

    Update VerificationCode.blade.php

--- resources/views/Auth/ForgotPassword/VerificationCode.blade.php
+++ resources/views/Auth/ForgotPassword/VerificationCode.blade.php
@@ -71,7 +71,7 @@
                                                 <h5 class="fs-3xl">Verifikasi Kode</h5>
                                                 <p class="text-muted mb-4">Masukkan kode verifikasi</p>
                                                 <div class="pb-4">
-                                                    <img src="{{('template/themesbrand.com/steex/layouts/assets/images/auth/email.png')}}" alt="" class="avatar-md">
+                                                    <img src="{{asset('template/themesbrand.com/steex/layouts/assets/images/auth/email.png')}}" alt="" class="avatar-md">
                                                 </div>
                                             </div>
                                             {{-- @if (isset($success) || (isset($error)))
@@ -106,7 +106,7 @@
                                                 </form>
                                             </div>
                                             <div class="mt-4 text-center">
-                                                <p class="mb-0">Tunggu, saya ingat kata sandi saya ... <a href="{{ url ('login')}}" class="fw-semibold text-primary text-decoration-underline"> Masuk </a> </p>
+                                                <p class="mb-0">Tunggu, saya ingat kata sandi saya ... <a href="{{ url ('auth/login')}}" class="fw-semibold text-primary text-decoration-underline"> Masuk </a> </p>
                                             </div>
                                         </div><!-- end card body -->
                                     </div><!-- end card -->
