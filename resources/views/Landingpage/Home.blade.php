dfbea9d05aa0f487eca2532e6d35f234244e0b99
jevonkagenou <kichironomercy@gmail.com>
2023-10-17 13:48:46 +0700

    Update

--- resources/views/Landingpage/Home.blade.php
+++ resources/views/Landingpage/Home.blade.php
@@ -423,6 +423,15 @@
     <script src="{{ asset('landingpage/js/main.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- Include SweetAlert library -->
+    <script>
+        function checkInternetConnection() {
+            var connected = window.navigator.onLine;
+            if (!connected) {
+                window.location.href = "/no-internet-connection";
+            }
+        }
+        checkInternetConnection();
+    </script>
 
 </body>
 
