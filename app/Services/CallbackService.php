ff08202d53f5ab98f8c14fac4164b546145e5168
jevonkagenou <kichironomercy@gmail.com>
2023-10-19 08:46:09 +0700

    Update app/Services

---
+++ app/Services/CallbackService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CallbackService
+{
+
+}
---
+++ app/Services/DetailTransactionService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+class DetailTransactionService
+{
+    public function detail($reference)
+    {
+
+        $apiKey = env('TRIPAY_API_KEY');
+
+        $payload = ['reference'    => $reference];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_FRESH_CONNECT  => true,
+            CURLOPT_URL            => 'https://tripay.co.id/api-sandbox/transaction/detail?' . http_build_query($payload),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $apiKey],
+            CURLOPT_FAILONERROR    => false,
+            CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+        ]);
+
+        $response = curl_exec($curl);
+        $error = curl_error($curl);
+
+        curl_close($curl);
+
+        return $response ? $response : $error;
+    }
+}
