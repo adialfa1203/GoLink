cfd53cfb643ee9729ffbea6859a2195e4dfc9710
Gmbs <gmbsstok@gmail.com>
2023-10-19 19:12:17 +0700

    Update CheckoutProduct.blade.php

--- resources/views/Subscribe/CheckoutProduct.blade.php
+++ resources/views/Subscribe/CheckoutProduct.blade.php
@@ -1,52 +1,126 @@
 @extends('layout.user.app')
 
 @section('title', 'Berlangganan')
+@section('style')
+<style>
+    .page-content {
+        background: #fff;
+    }
+
+    @media (min-width: 1025px) {
+
+        /* Atur gaya CSS khusus untuk perangkat laptop di sini */
+        .pay {
+            /* style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 1rem;" */
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            gap: 1rem;
+        }
+    }
+
+    @media (min-width: 601px) and (max-width: 1024px) {
+
+/* Atur gaya CSS khusus untuk perangkat tablet di sini */
+.pay {
+            /* style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 1rem;" */
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 1rem;
+        }
+}
+
+@media (max-width: 768px) {
+
+/* Atur gaya CSS khusus untuk perangkat tablet di sini */
+.pay {
+            /* style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 1rem;" */
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 1rem;
+        }
+}
+</style>
+@endsection
 @section('content')
 
-    <div class="page-content">
-        <div class="container-fluid">
-            <!--- Content1 --->
-            <div class="row">
-                <div class="col-lg-6 mb-4">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                <h4 class="mb-0">Pilih Metode Pembayaran</h4>
-                            </div>
+<div class="page-content">
+    <div class="container-fluid">
+        <!--- Content1 --->
+        <div class="row">
+            <div class="col-lg-6 mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                            <h4 class="mb-0">Pilih Metode Pembayaran</h4>
                         </div>
                     </div>
-                    <div class="card" style="border-radius: 20px;">
-                        <div class="card-body">
-                            <div class="row">
-                                @foreach ($channels as $channel)
-                                    <div class="card-animate col-lg-4 col-md-6 col-6 mt-3">
-                                        <form action="{{ route('payment') }}" method="POST">
-                                            @csrf
-                                            <input type="hidden" name="subscribe_id" value="{{ $subscribe->id }}">
+                </div>
+                <div class="card" style="border-radius: 20px;background: #F0F0F0;">
+                    <div class="card-body">
+                        <h4 class="card-title" style="color:#0E2954;">Pilih metode pembayaran anda untuk melakukan</h4>
+                        <h4 class="card-title mb-4" style="color:#0E2954;">Transaksi</h4>
+                        <h4 class="card-title mb-3 mt-5" style="color:#0E2954;">E-Wallet</h4>
+                        <div class="pay">
+                            @foreach ($channels as $channel)
+                            @if ($channel->group == 'E-Wallet')
+                            <div class="" style="border-radius: 30px;height: 10%;padding: 6px;">
+                                <form action="{{ route('payment') }}" method="POST">
+                                    @csrf
+                                    <!-- <input type="hidden" name="subscribe_id" value="{{ $subscribe->id }}">
                                             <input type="hidden" name="method" value="{{ $channel->code }}">
                                             <button type="submit" class="card border-0">
                                                 <img class="card-img-top img-fluid" src="{{ $channel->icon_url }}"
                                                     style="width: 100%;" alt="Card image cap">
-                                                <h6 class="card-title mt-2">{{ $channel->name }}</h6>
-                                            </button>
-                                        </form>
-                                    </div>
-                                @endforeach
+                                            </button>-->
+
+                                    <input type="hidden" name="subscribe_id" value="{{ $subscribe->id }}">
+                                    <input type="hidden" name="method" value="{{ $channel->code }}">
+                                    <button type="submit" class="card " style="background: #F0F0F0; border: 1.5px solid #0E2954;border-radius: 9px;height: 100%;padding: 6px;margin: 0;">
+                                        <img class="card-img-top img-fluid" src="{{ $channel->icon_url }}" style="object-fit: contain; width: 100px;height: 30px;" alt="Card image cap">
+                                    </button>
+                                </form>
                             </div>
+                            @endif
+                            @endforeach
                         </div>
+                        <h4 class="card-title mb-3 mt-3" style="color:#0E2954;">E-Wallet</h4>
+                        <div class="pay">
+                            @foreach ($channels as $channel)
+                            @if ($channel->group == 'Virtual Account')
+                            <div class="" style="border-radius: 30px;height: 10%;padding: 6px;">
+                                <form action="{{ route('payment') }}" method="POST">
+                                    @csrf
+                                    <!-- <input type="hidden" name="subscribe_id" value="{{ $subscribe->id }}">
+                                            <input type="hidden" name="method" value="{{ $channel->code }}">
+                                            <button type="submit" class="card border-0">
+                                                <img class="card-img-top img-fluid" src="{{ $channel->icon_url }}"
+                                                    style="width: 100%;" alt="Card image cap">
+                                            </button>-->
+
+                                    <input type="hidden" name="subscribe_id" value="{{ $subscribe->id }}">
+                                    <input type="hidden" name="method" value="{{ $channel->code }}">
+                                    <button type="submit" class="card " style="background: #F0F0F0;border: 1.5px solid #0E2954;border-radius: 9px;height: 100%;padding: 6px; margin: 0;">
+                                        <img class="card-img-top img-fluid" src="{{ $channel->icon_url }}" style="object-fit: contain; width: 100px;height: 30px;" alt="Card image cap">
+                                    </button>
+                                </form>
+                            </div>
+                            @endif
+                            @endforeach
+                        </div>
+
                     </div>
                 </div>
+            </div>
 
-                <!--- Content2 --->
-                <div class="col-lg-6 mb-4">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                <h4 class="mb-0">Langganan Pembelian</h4>
-                            </div>
+            <!--- Content2 --->
+            <div class="col-lg-6 mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                            <h4 class="mb-0">Langganan Pembelian</h4>
                         </div>
                         <div class="col-12">
-                            <div class="card card-animate" style="border-radius: 20px;">
+                            <div class="card card-animate" style="border-radius: 20px;background: #F0F0F0;">
                                 <div class="card-body">
                                     <h4 class="card-title mb-4" style="color:#0E2954;">Detail Layanan
                                         @if (strtolower(trim($subscribe->tipe)) === 'free')
@@ -108,7 +182,8 @@
                 </div>
             </div>
         </div>
+    </div>
 
-    </div>
-    </div>
+</div>
+</div>
 @endsection
