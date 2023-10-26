5a157ae16d9adb0ee91d7440b20c7d57f28ce515
adi alfa <adialfa1203@gmail.com>
2023-10-26 16:57:30 +0700

    block and unblock mail

--- resources/views/Banned/unblockMessage.blade.php
+++ resources/views/Banned/unblockMessage.blade.php
@@ -8,12 +8,13 @@
         body {
             font-family: Arial, sans-serif;
             background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
         }
         .container {
             max-width: 600px;
             margin: 0 auto;
             background-color: #fff;
-            padding: 20px;
             border: 1px solid #ddd;
             border-radius: 5px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -21,52 +22,93 @@
         .logo {
             display: block;
             margin: 0 auto;
+            max-width: 100%;
         }
         .header {
             text-align: center;
-            background-color: #00ACEE;
+            background-color: #088C0D;
             color: #fff;
-            padding: 20px;
+            padding: 10px 0;
             border-top-left-radius: 5px;
             border-top-right-radius: 5px;
+            height: 110px;
+        }
+        .header-content {
+            text-align: center;
         }
         .title {
             font-size: 24px;
             margin-bottom: 20px;
+            text-align: center;
         }
         .content {
             padding: 20px;
         }
-        ul {
+        .help {
+            text-align: center;
+            background-color: #0E2954;
+            color: #fff;
+            padding: 15px;
+            border-radius: 15px;
+        }
+        .ul {
             font-size: 16px;
             padding-left: 20px;
         }
         .footer {
             text-align: center;
-            background-color: #00ACEE;
-            color: #fff;
+            background-color: #fff;
+            color: grey;
             padding: 10px 0;
             border-bottom-left-radius: 5px;
             border-bottom-right-radius: 5px;
         }
+        .center-button {
+            text-align: center;
+        }
     </style>
 </head>
 <body>
-    <div class="container">
-        <img src="https://i.postimg.cc/G2LD1M6D/Logo-740-740-piksel-5.png" alt="Logo" class="logo" width="200">
-        <div class="header">
-            <h1>Akun Diaktifkan Kembali</h1>
-        </div>
-        <div class="content">
-            <h2 class="title">Pemberitahuan Akun Diaktifkan Kembali</h2>
-            <p>Kami ingin memberitahu Anda bahwa akun Anda telah berhasil diaktifkan kembali.</p>
-            <p>Anda sekarang dapat mengakses dan menggunakan akun Anda seperti biasa.</p>
-            <p>Jika Anda memiliki pertanyaan atau masalah, jangan ragu untuk menghubungi tim dukungan kami.</p>
-            <p>Terima kasih atas pemahaman Anda.</p>
-        </div>
-        <div class="footer">
-            <p>Ini adalah email pemberitahuan otomatis. Mohon tidak membalas email ini.</p>
-        </div>
-    </div>
+    <center>
+        <table class="container" cellspacing="0" cellpadding="0">
+            <tr>
+                <td>
+                    <div class="header">
+                        <img src="https://i.postimg.cc/1XsHQTZS/Logo-740-740-piksel-9.png" alt="Logo" style="margin-bottom: -20px" class="logo" width="70">
+                        <div class="header-content">
+                            <h4>Akun Diaktifkan Kembali</h4>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="content">
+                        <h2 class="title">Pemberitahuan Akun Diaktifkan Kembali</h2>
+                        <p>Kami ingin memberitahu Anda bahwa akun Anda telah berhasil diaktifkan kembali.</p>
+                        <p>Anda sekarang dapat mengakses dan menggunakan akun Anda seperti biasa.</p>
+                        <p>Jika Anda memiliki pertanyaan atau masalah, jangan ragu untuk menghubungi tim dukungan kami.</p>
+                        <p>Terima kasih atas pemahaman Anda.</p>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="center-button">
+                    <button class="help">
+                        <p style="padding: 0px; margin: 0px">
+                            <a style="color: #fff" href="https://go-link.trialdy.me/id/help-support">Bantuan dan Dukungan</a>
+                        </p>
+                    </button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="footer">
+                        <p style="font-size: 12px;">Ini adalah email pemberitahuan otomatis. Mohon tidak membalas email ini.</p>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </center>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Pemberitahuan Pemblokiran Akun</title>
+    <title>Pemberitahuan Akun Diaktifkan Kembali</title>
     <style>
         /* Tambahkan gaya CSS di sini */
         body {
             font-family: Arial, sans-serif;
             background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
         }
         .container {
             max-width: 600px;
             margin: 0 auto;
             background-color: #fff;
-            padding: 20px;
             border: 1px solid #ddd;
             border-radius: 5px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -21,29 +22,30 @@
         .logo {
             display: block;
             margin: 0 auto;
+            max-width: 100%;
         }
         .header {
             text-align: center;
-            background-color: #FF2323;
+            background-color: #088C0D;
             color: #fff;
-            padding: 2px;
+            padding: 10px 0;
             border-top-left-radius: 5px;
             border-top-right-radius: 5px;
-            display: flex;
-            align-items: center;
+            height: 110px;
         }
         .header-content {
-          margin-right: 150px;  /* Atur jarak antara gambar dan teks sesuai keinginan Anda */
+            text-align: center;
         }
         .title {
             font-size: 24px;
             margin-bottom: 20px;
+            text-align: center;
         }
         .content {
             padding: 20px;
         }
-        .help{
-          text-align: center;
+        .help {
+            text-align: center;
             background-color: #0E2954;
             color: #fff;
             padding: 15px;
@@ -61,37 +63,52 @@
             border-bottom-left-radius: 5px;
             border-bottom-right-radius: 5px;
         }
+        .center-button {
+            text-align: center;
+        }
     </style>
 </head>
 <body>
-    <div class="container">
-    <div class="header">
-        <img src="https://i.postimg.cc/g0Gdh314/Logo-740-740-piksel-8.png" alt="Logo" class="logo" width="80" style="width: 8%; margin-right: 15px;">
-        <div class="header-content">
-            <h2>Akun Anda Diblokir</h2>
-        </div>
-    </div>    
-        <div class="content">
-            <h2 class="title">Pemberitahuan Pemblokiran Akun</h2>
-            <p>Kami ingin memberitahu Anda bahwa akun Anda telah diblokir karena melanggar kebijakan penggunaan MiLink.id. Berikut adalah alasan pemblokiran :</p>
-            <ul>
-                <li>Penyalahgunaan tautan untuk tujuan ilegal.</li>
-                <li>Spam tautan berulang kali.</li>
-                <li>Melanggar hak cipta dengan tautan yang dibagikan.</li>
-            </ul>
-            <p>Jika Anda merasa pemblokiran ini adalah kesalahan atau ingin mengajukan banding, silakan hubungi tim dukungan kami segera untuk klarifikasi lebih lanjut.</p>
-            <p>Terima kasih atas pengertian Anda.</p>
-        </div>
-        <center>
-        <button class="help">
-          <p style="padding: 0px; margin: 0px">
-              <a style="color: #fff" href="https://go-link.trialdy.me/id/help-support">Bantuan dan Dukungan</a>
-          </p>
-        </button>
-        </center> 
-        <div class="footer">
-            <p style="font-size: 12px;">Ini adalah email pemberitahuan otomatis. Mohon tidak membalas email ini.</p>
-        </div>
-    </div>
+    <center>
+        <table class="container" cellspacing="0" cellpadding="0">
+            <tr>
+                <td>
+                    <div class="header">
+                        <img src="https://i.postimg.cc/1XsHQTZS/Logo-740-740-piksel-9.png" alt="Logo" style="margin-bottom: -20px" class="logo" width="70">
+                        <div class="header-content">
+                            <h4>Akun Diaktifkan Kembali</h4>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="content">
+                        <h2 class="title">Pemberitahuan Akun Diaktifkan Kembali</h2>
+                        <p>Kami ingin memberitahu Anda bahwa akun Anda telah berhasil diaktifkan kembali.</p>
+                        <p>Anda sekarang dapat mengakses dan menggunakan akun Anda seperti biasa.</p>
+                        <p>Jika Anda memiliki pertanyaan atau masalah, jangan ragu untuk menghubungi tim dukungan kami.</p>
+                        <p>Terima kasih atas pemahaman Anda.</p>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="center-button">
+                    <button class="help">
+                        <p style="padding: 0px; margin: 0px">
+                            <a style="color: #fff" href="https://go-link.trialdy.me/id/help-support">Bantuan dan Dukungan</a>
+                        </p>
+                    </button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="footer">
+                        <p style="font-size: 12px;">Ini adalah email pemberitahuan otomatis. Mohon tidak membalas email ini.</p>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </center>
 </body>
 </html>
