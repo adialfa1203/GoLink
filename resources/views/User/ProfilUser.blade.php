82eea122c2bfade2e5e97a83699b6dba1859dadf
adi alfa <adialfa1203@gmail.com>
2023-10-30 13:00:19 +0700

    fix bug profil

--- resources/views/User/ProfilUser.blade.php
+++ resources/views/User/ProfilUser.blade.php
@@ -150,14 +150,14 @@
                                         <div class="d-flex row">
                                             <div class="col-12 col-lg-2 col-xl-2 col-sm-6 col-md-6">
                                                 <center>
-                                                    @if ($user->profile_picture)
-                                                        <img src="{{ asset('profile_pictures/' . $user->profile_picture) }}" alt="{{ $user->name }}"
+                                                    @if (Auth::user()->profile_picture)
+                                                        <img src="{{ asset('profile_pictures/' . Auth::user()->profile_picture) }}" alt="{{ Auth::user()->name }}"
                                                             class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                    @elseif ($user->google_id)
-                                                        <img src="{{ $user->profile_picture }}" alt="{{ $user->name }}"
+                                                    @elseif (Auth::user()->google_id)
+                                                        <img src="{{ Auth::user()->profile_picture }}" alt="{{ Auth::user()->name }}"
                                                             class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
                                                     @else
-                                                        <img src="{{ asset('default/default.jpg') }}" alt="{{ $user->name }}"
+                                                        <img src="{{ asset('default/default.jpg') }}" alt="{{ Auth::user()->name }}"
                                                             class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
                                                     @endif
                                                     <input id="profile-img-file-input" name="profile_picture" type="file"
