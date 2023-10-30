dffc2d0cdc7f0825c50895112811bcddae3916c9
adi alfa <adialfa1203@gmail.com>
2023-10-30 13:06:49 +0700

    fix bug profil

--- resources/views/User/ProfilUser.blade.php
+++ resources/views/User/ProfilUser.blade.php
@@ -150,16 +150,20 @@
                                         <div class="d-flex row">
                                             <div class="col-12 col-lg-2 col-xl-2 col-sm-6 col-md-6">
                                                 <center>
-                                                    @if (Auth::user()->profile_picture)
-                                                        <img src="{{ asset('profile_pictures/' . Auth::user()->profile_picture) }}" alt="{{ Auth::user()->name }}"
-                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                    @elseif (Auth::user()->google_id)
-                                                        <img src="{{ Auth::user()->profile_picture }}" alt="{{ Auth::user()->name }}"
-                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                    @else
-                                                        <img src="{{ asset('default/default.jpg') }}" alt="{{ Auth::user()->name }}"
-                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
+                                                @if ($user->profile_picture)
+                                                @if($user->google_id)
+                                                    <img src="{{ $user->profile_picture }}" alt="{{ $user->name }}"
+                                                    class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
+                                                @else
+                                                    <img src="{{ asset('profile_pictures/' . $user->profile_picture) }}"
+                                                        alt="{{ $user->name }}"
+                                                        class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
                                                     @endif
+                                                @else
+                                                    <img src="{{ asset('default/default.jpg') }}"
+                                                        alt="{{ $user->name }}"
+                                                        class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
+                                                @endif
                                                     <input id="profile-img-file-input" name="profile_picture" type="file"
                                                         class="profile-img-file-input d-none">
                                                     <label for="profile-img-file-input" class="profile-photo-edit avatar-xs">
