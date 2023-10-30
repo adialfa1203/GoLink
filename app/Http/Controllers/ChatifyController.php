d6580b684c6f39b0d73c5a614e2265b0eb0b7786
jevonkagenou <kichironomercy@gmail.com>
2023-10-30 11:23:37 +0700

    Create ChatifyController.php

---
+++ app/Http/Controllers/ChatifyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChMessage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatifyController extends Controller
+{
+    public function setAllMessagesSeen()
+    {
+        $userId = Auth::id();
+        ChMessage::where('to_id', $userId)
+            ->update(['seen' => true]);
+        return redirect()->back();
+    }
+}
