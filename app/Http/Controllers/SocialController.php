731b8ae30974b1bfe52314c8e749000e04773ad2
Gmbs <gmbsstok@gmail.com>
2023-10-31 11:59:24 +0700

    Update SocialController.php

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -11,7 +11,8 @@ use Ramsey\Uuid\Uuid;
 
 class SocialController extends Controller
 {
-    public function redirectGoogle(){
+    public function redirectGoogle()
+    {
         return Socialite::driver('google')->redirect();
     }
 
@@ -44,13 +45,19 @@ class SocialController extends Controller
             if ($roleUser) {
                 $newUser->assignRole($roleUser);
             }
-
+            if ($user->is_banned) {
+                Auth::logout();
+                return redirect('id/login')->with('error', 'Akun Anda telah dibanned. Silakan hubungi admin untuk informasi lebih lanjut.');
+            } else {
+                return redirect()->route('dashboard.user')->withCookie(cookie('remember_web', true, 3));
+            }
             Auth::login($newUser);
             return redirect()->route('dashboard.user');
         }
     }
 
-    public function redirectFacebook(){
+    public function redirectFacebook()
+    {
         return Socialite::driver('facebook')->redirect();
     }
 
@@ -88,80 +95,80 @@ class SocialController extends Controller
         }
     }
 
-//     public function redirectTwitter(){
-//         return Socialite::driver('twitter')->redirect();
-//     }
+    //     public function redirectTwitter(){
+    //         return Socialite::driver('twitter')->redirect();
+    //     }
 
-//     public function twitterCallback()
-//     {
-//         $twitterUser = Socialite::driver('twitter')->user();
-//         $user = User::where('email', '=', $twitterUser->email)->first();
+    //     public function twitterCallback()
+    //     {
+    //         $twitterUser = Socialite::driver('twitter')->user();
+    //         $user = User::where('email', '=', $twitterUser->email)->first();
 
-//         if ($user) {
-//             Auth::login($user);
-//             return redirect()->route('dashboard.user');
-//         } else {
-//             $newUser = User::create([
-//                 'name' => $twitterUser->name,
-//                 'email' => $twitterUser->getEmail(),
-//                 'number' => $twitterUser->number ?? 'default_number',
-//                 'password' => bcrypt('12345678'),
-//                 'twitter_id' => $twitterUser->id,
-//                 'profile_picture' => 'default.jpg'
-//             ]);
+    //         if ($user) {
+    //             Auth::login($user);
+    //             return redirect()->route('dashboard.user');
+    //         } else {
+    //             $newUser = User::create([
+    //                 'name' => $twitterUser->name,
+    //                 'email' => $twitterUser->getEmail(),
+    //                 'number' => $twitterUser->number ?? 'default_number',
+    //                 'password' => bcrypt('12345678'),
+    //                 'twitter_id' => $twitterUser->id,
+    //                 'profile_picture' => 'default.jpg'
+    //             ]);
 
-//             if (!Role::where('name', 'user')->exists()) {
-//                 Role::create(['name' => 'user', 'guard_name' => 'web']);
-//             }
+    //             if (!Role::where('name', 'user')->exists()) {
+    //                 Role::create(['name' => 'user', 'guard_name' => 'web']);
+    //             }
 
-//             $roleUser = Role::where('name', 'user')->first();
+    //             $roleUser = Role::where('name', 'user')->first();
 
-//             if ($roleUser) {
-//                 $newUser->assignRole($roleUser);
-//             }
+    //             if ($roleUser) {
+    //                 $newUser->assignRole($roleUser);
+    //             }
 
-//             Auth::login($newUser);
-//             return redirect()->route('dashboard.user');
-//         }
-//     }
+    //             Auth::login($newUser);
+    //             return redirect()->route('dashboard.user');
+    //         }
+    //     }
 
-//     public function redirectGithub(){
-//         return Socialite::driver('github')->redirect();
-//     }
+    //     public function redirectGithub(){
+    //         return Socialite::driver('github')->redirect();
+    //     }
 
-//     public function githubCallback()
-//     {
-//         $githubUser = Socialite::driver('github')->user();
-//         $user = User::where('name', '=', $githubUser->name)->first();
+    //     public function githubCallback()
+    //     {
+    //         $githubUser = Socialite::driver('github')->user();
+    //         $user = User::where('name', '=', $githubUser->name)->first();
 
-//         if ($user) {
-//             Auth::login($user);
-//             return redirect()->route('dashboard.user');
-//         } else {
-//             $existingUser = User::where('name', '=', $githubUser->name)->first();
+    //         if ($user) {
+    //             Auth::login($user);
+    //             return redirect()->route('dashboard.user');
+    //         } else {
+    //             $existingUser = User::where('name', '=', $githubUser->name)->first();
 
-//             if ($existingUser) {
-//             } else {
-//                 $newUser = User::create([
-//                     'name' => $githubUser->name,
-//                     'profile_picture' => 'default.jpg'
-//                 ]);
+    //             if ($existingUser) {
+    //             } else {
+    //                 $newUser = User::create([
+    //                     'name' => $githubUser->name,
+    //                     'profile_picture' => 'default.jpg'
+    //                 ]);
 
-//                 if (!Role::where('name', 'user')->exists()) {
-//                     Role::create(['name' => 'user', 'guard_name' => 'web']);
-//                 }
+    //                 if (!Role::where('name', 'user')->exists()) {
+    //                     Role::create(['name' => 'user', 'guard_name' => 'web']);
+    //                 }
 
-//                 $roleUser = Role::where('name', 'user')->first();
+    //                 $roleUser = Role::where('name', 'user')->first();
 
-//                 if ($roleUser) {
-//                     $newUser->assignRole($roleUser);
-//                 }
+    //                 if ($roleUser) {
+    //                     $newUser->assignRole($roleUser);
+    //                 }
 
-//                 Auth::login($newUser);
-//             }
+    //                 Auth::login($newUser);
+    //             }
 
-//             return redirect()->route('dashboard.user');
-//         }
-//     }
+    //             return redirect()->route('dashboard.user');
+    //         }
+    //     }
 
 }
