5baa0227406a85dc68281e7d3ad3567ef0c8f00a
Gmbs <gmbsstok@gmail.com>
2023-10-31 15:10:24 +0700

    Update DataUserAdmin.blade.php

--- resources/views/Admin/DataUserAdmin.blade.php
+++ resources/views/Admin/DataUserAdmin.blade.php
@@ -310,7 +310,21 @@
                                                 <div class="col-sm-auto mt-3 mt-sm-0">
                                                     <div class="pagination-block pagination pagination-separated justify-content-center justify-content-sm-end mb-sm-0">
                                                         <div class="page-item">
-                                                        {{ $d->appends(['page_banned' => $bannedUser->currentPage()])->forPage($bannedUser->currentPage(), 4)->links('pagination::bootstrap-5', ['id' => 'dPagination']) }}
+                                                            @php
+        // Konversi koleksi Eloquent menjadi array
+        $data = $d->toArray();
+
+        // Buat objek LengthAwarePaginator dengan jumlah tautan halaman yang diinginkan
+        $perPage = 4; // Ganti sesuai kebutuhan
+        $currentPage = $bannedUser->currentPage();
+        $paginator = new \Illuminate\Pagination\LengthAwarePaginator(
+          $data, $d->count(), $perPage, $currentPage,
+          ['path' => route('route.name')] // Ganti dengan nama rute yang sesuai
+        );
+        @endphp
+
+{{ $paginator->appends(['page_banned' => $bannedUser->currentPage()])->links('pagination::bootstrap-5', ['id' => 'dPagination']) }}
+
                                                             <!-- {{ $d->appends(['page_banned' => $bannedUser->currentPage()])->onEachSide(1)->links('pagination::bootstrap-5', ['id' => 'dPagination']) }} -->
                                                         </div>
                                                     </div>
