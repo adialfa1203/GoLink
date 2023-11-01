ac953a057463acac6d5c6a143709fbc87a11646c
jevonkagenou <kichironomercy@gmail.com>
2023-11-01 13:30:07 +0700

    Update ProfilUser.blade.php

--- resources/views/User/ProfilUser.blade.php
+++ resources/views/User/ProfilUser.blade.php
@@ -155,27 +155,21 @@
                                             <div class="col-12 col-lg-2 col-xl-2 col-sm-6 col-md-6">
                                                 <center>
                                                     @if ($user->profile_picture)
-                                                        @if ($user->google_id)
-                                                            <img src="{{ asset('profile_pictures/' . $user->profile_picture) }}"
-                                                                alt="{{ $user->name }}"
-                                                                class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                        @else
-                                                            <img src="{{ $user->profile_picture }}"
-                                                                alt="{{ $user->name }}"
-                                                                class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                        @endif
-                                                    @else
-                                                        @if ($user->google_id)
-                                                            <img src="{{ $user->profile_picture }}"
+                                                        <img src="{{ asset('profile_pictures/' . $user->profile_picture) }}"
                                                             alt="{{ $user->name }}"
                                                             class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                        @else
-                                                            <img src="{{ asset('default/default.jpg') }}"
-                                                                alt="{{ $user->name }}"
-                                                                class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                        @endif
+                                                    @elseif ($user->google_id)
+                                                        <img src="{{ $user->profile_picture }}" alt="{{ $user->name }}"
+                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
+                                                    @elseif ($user->google_id && $user->updated_profile_picture)
+                                                        <img src="{{ asset('profile_pictures/' . $user->profile_picture) }}"
+                                                            alt="{{ $user->name }}"
+                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
+                                                    @else
+                                                        <img src="{{ asset('default/default.jpg') }}"
+                                                            alt="{{ $user->name }}"
+                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
                                                     @endif
-
                                                     <input id="profile-img-file-input" name="profile_picture" type="file"
                                                         class="profile-img-file-input">
                                                     <label for="profile-img-file-input"
