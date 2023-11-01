a7027f85ab97ce2fb8aaa25f42da63c1db7e8fdc
jevonkagenou <kichironomercy@gmail.com>
2023-11-01 11:44:03 +0700

    Update ProfilUser.blade.php

--- resources/views/User/ProfilUser.blade.php
+++ resources/views/User/ProfilUser.blade.php
@@ -123,6 +123,7 @@
             border-radius: 0;
             background-color: transparent;
         }
+
         .page-content {
             background: #ffffff;
         }
@@ -154,9 +155,10 @@
                                             <div class="col-12 col-lg-2 col-xl-2 col-sm-6 col-md-6">
                                                 <center>
                                                     @if ($user->profile_picture)
-                                                        @if($user->google_id)
-                                                            <img src="{{ $user->profile_picture }}" alt="{{ $user->name }}"
-                                                            class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
+                                                        @if ($user->google_id)
+                                                            <img src="{{ $user->profile_picture }}"
+                                                                alt="{{ $user->name }}"
+                                                                class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
                                                         @else
                                                             <img src="{{ asset('profile_pictures/' . $user->profile_picture) }}"
                                                                 alt="{{ $user->name }}"
@@ -166,16 +168,17 @@
                                                         <img src="{{ asset('default/default.jpg') }}"
                                                             alt="{{ $user->name }}"
                                                             class="avatar-lg rounded-circle object-fit-cover border-0 img-thumbnail user-profile-image">
-                                                    @endif
-                                                    @if (!$user->google_id)
-                                                        <input id="profile-img-file-input" name="profile_picture" type="file"
-                                                            class="profile-img-file-input d-none">
-                                                        <label for="profile-img-file-input"
-                                                            class="profile-photo-edit avatar-xs">
-                                                            <span class="avatar-title rounded-circle bg-light text-body foto">
-                                                                <i class="bi bi-camera"></i>
-                                                            </span>
-                                                        </label>
+                                                        @if (!$user->google_id)
+                                                            <input id="profile-img-file-input" name="profile_picture"
+                                                                type="file" class="profile-img-file-input d-none">
+                                                            <label for="profile-img-file-input"
+                                                                class="profile-photo-edit avatar-xs">
+                                                                <span
+                                                                    class="avatar-title rounded-circle bg-light text-body foto">
+                                                                    <i class="bi bi-camera"></i>
+                                                                </span>
+                                                            </label>
+                                                        @endif
                                                     @endif
                                                 </center>
                                             </div>
