e8088202607a03384abd4566f7aa11d1a97491f5
adi alfa <adialfa1203@gmail.com>
2023-11-02 11:15:43 +0700

    page 404 anda 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,40 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Error 403 Forbidden </title>
+    <title>403 Error Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <style>
+        .center-content {
+            text-align: center;
+        }
+        .center-content img {
+            max-width: 100%;
+            max-height: 300px; /* Sesuaikan tinggi gambar sesuai kebutuhan */
+            display: block;
+            margin: 0 auto;
+        }
+    </style>
+    <style>
+        .custom-button {
+            background-color: #0E2954;
+            color: #fff;
+            padding: 8px 30px;
+            border-radius: 10px;
+        }
+    </style>
 </head>
-
 
 <body>
     <div class="d-flex align-items-center justify-content-center vh-100">
-        <div class="text-center">
-            <h1 class="display-1 fw-bold">403</h1>
-            <p class="fs-3"> <span class="text-danger">Maaf, Anda tidak memiliki izin untuk mengakses halaman
-                    ini.</span></p>
-            <p class="lead">
-                Server mengirim kode status HTTP 403 "Terlarang" saat server menolak akses ke sumber daya yang diminta
-                oleh klien karena alasan tertentu.
+        <div class="text-center center-content">
+            <img src="https://i.postimg.cc/vByXjXwr/403-Error-Forbidden-rafiki.png" alt="Gambar 404" />
+            <b class="fs-3"> <span class="text-danger">Maaf, Anda tidak memiliki izin untuk mengakses halaman
+                ini.</span></b>
+            <br>
+            <p class="font-weight-bold fs-5" style="color: #0E2954; font-family: Poppins">
+                Server menolak akses sumber daya yang diminta jarebna alasan tertentu
             </p>
             @auth
-                @if (auth()->user()->hasRole('admin'))
-                    <a class="btn btn-primary text-white" href="{{ route('dashboard.admin') }}">Kembali</a>
-                @elseif(auth()->user()->hasRole('user'))
-                    <a class="btn btn-primary text-white" href="{{ route('dashboard.user') }}">Kembali</a>
-                @else
-                    <a class="btn btn-primary text-white" href="{{ route('login') }}">Kembali ke Login</a>
-                @endif
+            @if (auth()->user()->hasRole('admin'))
+                <a class="btn custom-button text-white" href="{{ route('dashboard.admin') }}">Kembali</a>
+            @elseif(auth()->user()->hasRole('user'))
+                <a class="btn custom-button text-white" href="{{ route('dashboard.user') }}">Kembali</a>
             @else
-                <a class="btn btn-primary text-white" href="{{ route('login') }}">Kembali ke Login</a>
-            @endauth
+                <a class="btn custom-button text-white" href="{{ route('login') }}">Kembali ke Login</a>
+            @endif
+            @else
+                <a class="btn custom-button text-white" href="{{ route('login') }}">Kembali ke Login</a>
+            @endauth           
         </div>
     </div>
 </body>
-
 
 </html>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>404 Error Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <style>
         .center-content {
             text-align: center;
@@ -18,27 +19,35 @@
             margin: 0 auto;
         }
     </style>
+    <style>
+        .custom-button {
+            background-color: #0E2954;
+            color: #fff;
+            padding: 8px 30px;
+            border-radius: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="text-center center-content">
-            <img src="https://i.postimg.cc/DZxQM6wM/404.png" alt="Gambar 404" />
+            <img src="https://i.postimg.cc/263Nqz39/404-Error-pana.png" alt="Gambar 404" />
             <b class="fs-3"> <span class="text-danger">Tidak Ditemukan!</span></b>
             <br>
-            <p class="font-weight-bold fs-3" style="color: #0E2954">
-                Tautan yang anda akses telah ubah atau kadaluarsa.
+            <p class="font-weight-bold fs-5" style="color: #0E2954; font-family: Poppins">
+                Tautan yang anda kunjungi tidak ada atau telah kadaluarsa
             </p>
             @auth
                 @if (auth()->user()->hasRole('admin'))
-                    <a class="btn btn-primary text-white" href="{{ route('dashboard.admin') }}">Kembali</a>
+                    <a class="btn custom-button text-white" href="{{ route('dashboard.admin') }}">Kembali</a>
                 @elseif(auth()->user()->hasRole('user'))
-                    <a class="btn btn-primary text-white" href="{{ route('dashboard.user') }}">Kembali</a>
+                    <a class="btn custom-button text-white" href="{{ route('dashboard.user') }}">Kembali</a>
                 @else
-                    <a class="btn btn-primary text-white" href="{{ route('login') }}">Kembali ke Login</a>
+                    <a class="btn custom-button text-white" href="{{ route('login') }}">Kembali ke Login</a>
                 @endif
             @else
-                <a class="btn btn-primary text-white" href="{{ route('login') }}">Kembali ke Login</a>
+                <a class="btn custom-button text-white" href="{{ route('login') }}">Kembali ke Login</a>
             @endauth            
         </div>
     </div>
