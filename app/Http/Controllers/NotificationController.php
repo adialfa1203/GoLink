c7093b38244647053d2ccb7b2176bb337ee88551
jevonkagenou <kichironomercy@gmail.com>
2023-11-03 14:41:51 +0700

    Profile Done

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -81,22 +81,26 @@ class NotificationController extends Controller
             foreach ($ch_messages as $message) {
                 if ($message->fromUser) {
                     $message->fromUserName = $message->fromUser->name;
-                    if ($message->fromUser->google_id){
+                    if ($message->fromUser->google_id) {
                         $message->image = $message->fromUser->profile_picture;
+                    } elseif ($message->fromUser->profile_picture) {
+                        $message->image = config('app.url') . '/profile_pictures/' . $message->fromUser->profile_picture;
                     } else {
-                        $message->image = config('app.url').'/profile_pictures/'.$message->fromUser->profile_picture;
+                        $message->image = config('app.url') . '/default/default.jpg';
                     }
                 }
-            
+    
                 if ($message->toUser) {
                     $message->toUserName = $message->toUser->name;
-                    if ($message->toUser->google_id){
+                    if ($message->toUser->google_id) {
                         $message->image = $message->toUser->profile_picture;
+                    } elseif ($message->toUser->profile_picture) {
+                        $message->image = config('app.url') . '/profile_pictures/' . $message->toUser->profile_picture;
                     } else {
-                        $message->image = config('app.url').'/profile_pictures/'.$message->toUser->profile_picture;
+                        $message->image = config('app.url') . '/default/default.jpg';
                     }
-                }
-            }            
+                }            
+            }         
 
         // dd($ch_messages);
 
