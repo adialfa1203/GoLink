26b1dda41917a87a853ea78645b8a33dcb60481e
Gmbs <gmbsstok@gmail.com>
2023-11-29 13:31:51 +0700

    coba

--- resources/views/pdf/document.blade.php
+++ resources/views/pdf/document.blade.php
@@ -5,111 +5,145 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         .rata {
-            float: left;
-            font-family: Poppins;
+            text-align: left;
+            margin-top: 0;
+            font-family: sans-serif;
+            font-weight: lighter;
         }
 
         .rata1 {
-            float: right;
+            text-align: right;
+            margin-top: 0;
+            font-family: sans-serif;
+            font-weight: lighter;
         }
+        .ri-checkbox-circle-fill {
+  /* Gaya ikon checkbox */
+  display: inline-block;
+  width: 16px; /* Sesuaikan lebar ikon sesuai kebutuhan */
+  height: 16px; /* Sesuaikan tinggi ikon sesuai kebutuhan */
+  background-color: #28a745; /* Warna hijau sesuai dengan kelas "text-success" */
+  border-radius: 50%; /* Membuat sudut border menjadi lingkaran */
+
+  /* Gaya centang di dalam lingkaran */
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 8px; /* Sesuaikan lebar centang sesuai kebutuhan */
+    height: 3px; /* Sesuaikan tinggi centang sesuai kebutuhan */
+    border: 2px solid #fff; /* Warna putih untuk centang */
+    border-top: none;
+    border-left: none;
+    transform: rotate(-45deg); /* Rotasi centang */
+  }
+}
+
     </style>
 </head>
 
 <body>
     <div class="container mt-5">
-        <div class="card" style="background-color: #F0F0F0;">
-            <div class="row" style="padding: 30px;">
-                <div class="row">
-                    <div class="col-6">
-                        <h6 class="rata" style="color: #088C0D;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                                <path d="M14 6.70096C14 10.4018 10.866 13.4019 7 13.4019C3.134 13.4019 0 10.4018 0 6.70096C0 3.00011 3.134 0 7 0C10.866 0 14 3.00011 14 6.70096ZM6.19031 10.2491L11.3839 5.27738C11.5602 5.10856 11.5602 4.83482 11.3839 4.666L10.7452 4.05462C10.5688 3.88577 10.2829 3.88577 10.1065 4.05462L5.87097 8.10919L3.8935 6.21619C3.71714 6.04737 3.43119 6.04737 3.2548 6.21619L2.61614 6.82758C2.43978 6.9964 2.43978 7.27014 2.61614 7.43896L5.55162 10.249C5.728 10.4179 6.01393 10.4179 6.19031 10.2491Z" fill="#088C0D" />
-                            </svg>
+        <div class="card" style="background-color: #F0F0F0;padding: 30px; ">
+        <center>
+            <img src="template/image/milink-gelap.png" alt="logo" width="200" height="50" style="margin-bottom: 20px;">
+        </center>
+            <table class="table" style=" width: 100%;">
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata" style="color: #088C0D;margin-bottom: 0;font-weight: bold;">
+                        <i class="ri-checkbox-circle-fill text-success"></i>
                             Transaksi Berhasil
-                        </h6>
-                    </div>
-                    <div class="col-6">
-                        <h6 class="rata1">Bni</h6>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="col-6">
-                        <h6 class="rata text-muted">Telah Dibayar</h6>
-                    </div>
-                    <div class="col-6">
-                        <h6 class="rata1 text-muted">Rp. </h6>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <h5 class="rata">Total Bayar</h5>
-                    </div>
-                    <div class="col-6">
-                        <h6 class="rata1">Rp.200</h6>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <p class="rata">Harga</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="rata1">Rp.3000</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <p class="rata">Biaya admin</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="rata1">Rp.100</p>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="col-12">
-                        <h6 class="rata">Detail Pesanan</h6>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <p class="rata">Nama</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="rata1">wanto</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <p class="rata">Berlangganan</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="rata1">silver</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <p class="rata">Nomor Telepon</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="rata1">08225</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <p class="rata">Referensi</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="rata1">45415256894</p>
-                    </div>
-                </div>
-                <hr>
-                <p class="text-muted" style="margin: 0;">PT Hummatech Digital Indonesia</p>
-                <p class="text-muted" style="margin: 0;">Perum Permata Regency 1 Blok 10/28, Perun Gpa, Ngijo, Kec. Karang Ploso, Kabupaten Malang, Jawa Timur 65152</p>
-            </div>
+                        </h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1" style="font-weight:bold; margin-bottom: 0;">Bni</h5>
+                    </td>
+                </tr>
+            </table>
+            <hr>
+            <table class="table" style=" width: 100%;">
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata" style="color: #888888;">Telah Dibayar</h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1" style="color: #888888;">2023-10-10 </h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata" style="font-weight: bold;">Total Bayar</h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1" style="font-weight: bold;">Rp</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata">Harga </h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1">Rp</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata" style="margin-bottom: 0">Biaya Admin</h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1" style="margin-bottom: 0">Rp</h5>
+                    </td>
+                </tr>
+            </table>
+            <hr>
+            <table class="table" style=" width: 100%;">
+                <tr>
+                    <td>
+                        <h5 style="margin-top: 0;font-family: sans-serif;font-weight: bold;">Detail Pesanan</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata">Nama</h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1">Rp</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata">Berlangganan</h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1">Rp</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata">Nomor Telepon </h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1">Rp</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 50%;">
+                        <h5 class="rata" style="margin-bottom: 0">Referensi</h5>
+                    </td>
+                    <td style="text-align: right; padding-right: 20px;">
+                        <h5 class="rata1" style="margin-bottom: 0">Rp</h5>
+                    </td>
+                </tr>
+            </table>
+            <hr>
+            <h5 style="margin: 0;font-family: sans-serif;font-weight: lighter; color: #888888;">PT Hummatech Digital Indonesia</h5>
+            <h5 style="margin: 0;font-family: sans-serif;font-weight: lighter; color: #888888;">Perum Permata Regency 1 Blok 10/28, Perun Gpa, Ngijo, Kec. Karang Ploso, Kabupaten Malang, Jawa Timur 65152</h5>
         </div>
     </div>
 </body>
