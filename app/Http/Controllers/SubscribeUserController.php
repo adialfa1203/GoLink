d376357ef5e68d78b15b080f1e3b4518d55fd2af
adi alfa <adialfa1203@gmail.com>
2023-12-01 16:15:15 +0700

    Update SubscribeUserController.php

--- app/Http/Controllers/SubscribeUserController.php
+++ app/Http/Controllers/SubscribeUserController.php
@@ -82,7 +82,6 @@ class SubscribeUserController extends Controller
     public function detail($reference)
     {
         $detailTransaction = $this->service->detail($reference);
-        dd($detailTransaction);
         $detailTransaction = json_decode($detailTransaction);
         $transaction = Transaction::where('reference', $reference)
             ->with('subscribe')->first();
