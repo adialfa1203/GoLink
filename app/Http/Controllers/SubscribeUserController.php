a37941f4adca3c64ae13631e46bfbf804d363549
adi alfa <adialfa1203@gmail.com>
2023-12-01 16:07:08 +0700

    Update SubscribeUserController.php

--- app/Http/Controllers/SubscribeUserController.php
+++ app/Http/Controllers/SubscribeUserController.php
@@ -62,7 +62,6 @@ class SubscribeUserController extends Controller
 
         $transaction = $tripay->requestTransaction($method, $subscribe);
         $data = json_decode($transaction);
-        dd($data);
         $data = $data->data;
         Transaction::query()->create([
             'user_id' => auth()->user()->id,
