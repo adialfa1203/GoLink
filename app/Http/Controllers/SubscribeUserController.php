bda1ed7a3bb17507ffa9088bf8e1bdae17457a9f
adi alfa <adialfa1203@gmail.com>
2023-12-04 10:00:24 +0700

    Update SubscribeUserController.php

--- app/Http/Controllers/SubscribeUserController.php
+++ app/Http/Controllers/SubscribeUserController.php
@@ -87,7 +87,7 @@ class SubscribeUserController extends Controller
             ->with('subscribe')->first();
         $tripay = new TripayController();
         $foto1 = $tripay->getPaymentChannels();
-        $expired = Carbon::parse($detailTransaction->data->expired_time)->isoFormat('DD MMMM Y H:mm');
+        $expired = Carbon::parse($detailTransaction->data->expired_time)->isoFormat('DD MMMM Y H:mm:ss');
         return view('Subscribe.TransactionShow', compact('detailTransaction', 'transaction', 'foto1', 'expired'));
     }
     public function deleteTransaction($reference)
